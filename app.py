7adc37c28f1cbf5018c559e8448859fad8a721e8
Bruno Flaven <bflaven@gmail.com>
2021-10-15 16:20:55 +0200

    update app and requirements

--- app.py
+++ app.py
@@ -2,9 +2,19 @@
 # -*- coding: utf-8 -*-
 """
 [env]
+conda create --name heroku_python_getting_started_1 python=3.9.7
+conda info --envs
+source activate heroku_python_getting_started_1
+conda deactivate
+
+
 
 [path]
 cd /Users/brunoflaven/Documents/03_git/deploy-a-streamlit-app-with-heroku/
+
+- working copy
+cd /Users/brunoflaven/Documents/02_copy/_packt_copycat_coder/deploy-a-streamlit-app-with-heroku_1
+
 
 [file]
 streamlit run app.py
@@ -18,41 +28,57 @@ plotly-express==0.4.1
 altair==4.1.0
 pip==21.3
 
+# no needed
+# import seaborn as sns
+# import datetime as dt
+# import altair as alt
+
+
+# st.write("seaborn :: ", sns.__version__)
+# st.write("datetime :: ", dt.__version__)
+# st.write("altair :: ", dt.__version__)
+
+
 
 """
 # require in this file
 import streamlit as st
-# import numpy as np
-# import pandas as pd
-# import seaborn as sns
-# import datetime as dt
-# import matplotlib.pyplot as plt
-# import altair as alt
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
 
 
+
+def detectVersion():
+    st.sidebar.markdown('**VERSIONS**')
+    st.sidebar.write("panda ::", pd.__version__)
+    st.sidebar.write("numpy :: ", np.__version__)
+    
 
 
 def main():
 	""" A simple attempt for heroku"""
 	st.title('Attempt streamlit-dashboard app on Heroku')
-	st.write('Disable libraries, import just streamlit')
+	st.write('enable some libraries :: streamlitn, pandas, numpy, matplotlib')
  
-	# df = pd.DataFrame(
- #            np.random.randn(45, 3),
- #            columns=['John', 'Dave', 'Yuri']
- #        )
+  
+ 
+	df = pd.DataFrame(
+            np.random.randn(45, 3),
+            columns=['John', 'Dave', 'Yuri']
+        )
 
-	# columns = st.multiselect(
-	# 		label='What column to you want to display', options=df.columns)
+	columns = st.multiselect(
+			label='What column to you want to display', options=df.columns)
 
-	# #check if variable is empty
-	# if not columns:
-	# 	st.info("No column selected...")
-	# else:
-	# 	st.write(df[columns])
-	# 	fig, ax = plt.subplots()
-	# 	ax = plt.hist(df[columns])
-	# 	st.pyplot(fig)
+	#check if variable is empty
+	if not columns:
+		st.info("No column selected...")
+	else:
+		st.write(df[columns])
+		fig, ax = plt.subplots()
+		ax = plt.hist(df[columns])
+		st.pyplot(fig)
 
 	# if st.checkbox('Show another fake dataframe\'s example'):
 	# 	st.dataframe(pd.DataFrame({
@@ -62,6 +88,7 @@ def main():
  
  
 if __name__ == '__main__':
-	main()
+    main()
+    detectVersion()
 
 
